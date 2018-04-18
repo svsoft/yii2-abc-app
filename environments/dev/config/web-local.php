0f265b6ccd078c13cdd330f0cf092426c937ef3a
Viktor <shiryakovv@gmail.com>
2018-04-18 13:34:17 +0700

    update web-local.php

--- environments/dev/config/web-local.php
+++ environments/dev/config/web-local.php
@@ -10,6 +10,9 @@ $config =  [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
         ],
+        'assetManager' => [
+            'linkAssets' => false,
+        ],
     ],
 ];
 
